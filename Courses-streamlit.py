36ad24094e55d4a8e856a408a76ef701163d821d
Arjun Arora <32399220+1arjunarora@users.noreply.github.com>
2023-11-28 15:13:31 -0800

    Add files via upload

---
+++ Courses-streamlit.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+import streamlit as st
+import pdfplumber
+import os
+import openai
+from PIL import Image
+
+image_path = Image.open('logo.jpg')
+st.image(image_path)
+
+
+with st.sidebar:
+    
+    # About section
+    st.markdown("# Meet Kelly - Your AI Academic Advisor")
+    st.markdown(
+        "This AI Assistant allows you to share your academic interests and career goals, and "
+        "receive personalized recommendations for academic major, job potential and classes! " 
+        )
+    
+    # divider
+    st.markdown("---")
+
+    # how to use section
+    st.markdown(
+        "## How does this work? \n"
+        "Our AI assistant is integrated with the latest version of university curriculum, checksheets and coursework to help students\n"
+        "in quickly finding relevant and accurate insights to plan for their future. Please review your final plan with you academic advisor after! \n"
+    )
+    
+# Set your OpenAI API key
+openai.api_key = "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"
+
+def upload_pdf_and_retrieve_info(file_path, user_query):
+    # Read the PDF file and extract text content
+    with pdfplumber.open(file_path) as pdf:
+        pdf_text = ""
+        for page in pdf.pages:
+            pdf_text += page.extract_text()
+
+    # Set up a prompt with the extracted text
+    prompt = f"Step 1 - Act like an academic advisor at a university, be concise with your suggestions, and share a list of core classes and required courses in bullet format first and then answer the question. Step 2 - Start with thank you for asking the question and mention that  Students should meet with their advisors each semester to monitor their progress toward the graduation requirements. Step 3 - Retrieve information from the following PDF:\n{pdf_text}\n\nUser Query:"
+
+    # Combine prompt and user query
+    input_text = f"{prompt} {user_query} {max_label} "
+
+    # Call OpenAI's API for completion
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # You can choose another engine if needed
+        prompt=input_text,
+        temperature=0.2,
+        max_tokens=800,
+        n=1
+    )
+
+    # Extract and return the model's response
+    return response['choices'][0]['text']
+
+if __name__ == "__main__":
+    # Streamlit app
+  #  st.title("AI Powered Academic Advising")
+
+    # User input for max_label
+    max_label = st.text_input("Enter Business Major:", "For example Marketing, Accounting, Sports Management, etc.")
+
+    # Specify the file path directly
+    file_path = os.path.join(os.path.expanduser("~"), "Desktop\AI Assistant\checksheets", f"{max_label}.pdf")
+
+    # Get user query input
+    user_query = st.text_input("Enter your query:", "For example, what are some of the core classes I should take for entrepreneurship?")
+
+    # Process file and user query when a button is clicked
+    if st.button("Ask Kutztown University's Personalized AI Assistant"):
+        # Call the function to retrieve information
+        model_response = upload_pdf_and_retrieve_info(file_path, user_query)
+
+        # Display the model's response
+        st.subheader("AI Assistant's Response:")
+        st.write(model_response)
+
