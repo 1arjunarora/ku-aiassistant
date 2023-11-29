d560160af06d13238d953aa168766a72106bc753
Arjun Arora <32399220+1arjunarora@users.noreply.github.com>
2023-11-28 19:22:39 -0800

    Update Courses-streamlit.py

--- Courses-streamlit.py
+++ Courses-streamlit.py
@@ -1,3 +1,4 @@
+# import libraries
 import streamlit as st
 import pdfplumber
 import os
@@ -12,7 +13,9 @@ openai.api_key = "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"
 image_path = Image.open('logo.jpg')
 st.image(image_path)
 
+##########################################################################################################
 # Sidebar FAQ
+
 with st.sidebar:
     # About section
     st.markdown("# Meet Kelly - Your AI Academic Advisor")
@@ -31,6 +34,33 @@ with st.sidebar:
         "in quickly finding relevant and accurate insights to plan for their future. Please review your final plan with your academic advisor after! \n"
     )
 
+##########################################################################################################
+# main app section setup
+# List of options
+options = [
+    "Accounting",
+    "Finance",
+    "Entrepreneurship",
+    "General Business",
+    "Human Resource Management",
+    "Personal Financial Planning",
+    "Management",
+    "Marketing",
+    "Supply Chain Management",
+    "Sports Management"
+]
+
+# User inputs
+Selected_label = st.selectbox("Enter Your Preferred Major:", options)
+
+# seleect the file path of curriculum dynamically
+file_path = f"https://raw.githubusercontent.com/1arjunarora/ku-aiassistant/main/checksheets/{Selected_label}.pdf"
+
+# Get user query input
+user_query = st.text_input("Enter your query:", "As a freshman, what classes should I take? I am in my 3rd semester, what classes should I take?")
+
+##########################################################################################################
+
 def upload_pdf_and_retrieve_info(url, user_query):
     # Read the PDF file and extract text content
     with fitz.open(url) as pdf:
@@ -40,10 +70,10 @@ def upload_pdf_and_retrieve_info(url, user_query):
             pdf_text += page.get_text()
 
     # Set up a prompt with the extracted text
-    prompt = f"Step 1 - Act like an academic advisor at a university, be concise with your suggestions, and share a list of core classes and required courses in bullet format first and then answer the question. Step 2 - Start with thank you for asking the question and mention that  Students should meet with their advisors each semester to monitor their progress toward the graduation requirements. Step 3 - Retrieve information from the following PDF:\n{pdf_text}\n\nUser Query:"
+    prompt = f"Act like an academic advisor at a university, be concise with your suggestions, and share your answers in bullet format. Additionally, answer the question asked by student, by starting with saying thank you for asking the question and mention that Students should meet with their advisors each semester to monitor their progress towards graduation requirements. Lastly, retrieve information from the following PDF:\n{pdf_text}\n\nUser Query:"
 
     # Combine prompt and user query
-    input_text = f"{prompt} {user_query} {max_label} "
+    input_text = f"{prompt} {user_query} "
 
     # Call OpenAI's API for completion
     response = openai.Completion.create(
@@ -57,36 +87,12 @@ def upload_pdf_and_retrieve_info(url, user_query):
     # Extract and return the model's response
     return response['choices'][0]['text']
 
-if __name__ == "__main__":
+# Process file and user query when a button is clicked
+if st.button("Ask University's AI Assistant"):
 
-    # List of options
-    options = [
-        "Accounting",
-        "Finance",
-        "Entrepreneurship",
-        "General Business",
-        "Human Resource Management",
-        "Personal Financial Planning",
-        "Management",
-        "Marketing",
-        "Supply Chain Management",
-        "Sports Management"
-    ]
+    # Call the function to retrieve information
+    model_response = upload_pdf_and_retrieve_info(file_path, user_query)
 
-    # User input for max_label
-    max_label = st.selectbox("Enter Your Preferred Major:", options)
-
-    # Specify the file path directly
-    file_path = f"https://raw.githubusercontent.com/1arjunarora/ku-aiassistant/main/checksheets/{max_label}.pdf"
-
-    # Get user query input
-    user_query = st.text_input("Enter your query:", "As a freshman, what classes should I take? I am in my 3rd semester, what classes should I take?")
-
-    # Process file and user query when a button is clicked
-    if st.button("Ask Kutztown University's Custom AI Assistant"):
-        # Call the function to retrieve information
-        model_response = upload_pdf_and_retrieve_info(file_path, user_query)
-
-        # Display the model's response
-        st.subheader("AI Assistant's Response:")
-        st.write(model_response)
+    # Display the model's response
+    st.subheader("AI Assistant's Response:")
+    st.write(model_response)
